6a7c0c6050d7f1880c20eeef4b9f83ca8dfe0298
Rustam Shangareev <rustam.letitbe@gmail.com>
2025-02-18 14:11:03 +0300

    feat: organize __init__.py file for data providers module

---
+++ ts_flow/core/data_providers/__init__.py
@@ -0,0 +1,5 @@
+from .abstract import DataProvider, T
+from .simple_data_provider import SimpleDataProvider
+from .file_data_provider import FileDataProvider
+
+__all__ = ["DataProvider", "SimpleDataProvider", "FileDataProvider", "T"]
