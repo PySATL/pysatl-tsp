53cd6c75862e7bdeb8325ff3d0bb769ce3f9e4b4
Rustam Shangareev <rustam.letitbe@gmail.com>
2025-03-09 15:00:23 +0300

    feat: make processor library and implement mapping time series processor

---
+++ ts_flow/core/processor/mapping_handler.py
@@ -0,0 +1,18 @@
+from ts_flow.core import Handler, T, U
+from typing import Callable, Optional, Any, Iterable, Iterator
+
+class MappingHandler(Handler[T, U]):
+    def __init__(
+        self,
+        map_func: Callable[[T], U],
+        source: Optional[Handler[Any, T]] = None
+    ):
+        super().__init__(source)
+        self.map_func = map_func
+
+    def __iter__(self) -> Iterator[U]:
+        if self.source is None:
+            raise ValueError("Source is not set")
+        
+        for segment in self.source:
+            yield self.map_func(segment)
