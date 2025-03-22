0e7f3da5e15cddd1401d5927aa0a6991730d5056
Rustam Shangareev <rustam.letitbe@gmail.com>
2025-03-22 23:17:04 +0300

    refactor: mapping handler refactor

--- ts_flow/core/processor/mapping_handler.py
+++ ts_flow/core/processor/mapping_handler.py
@@ -1,18 +1,17 @@
+from collections.abc import Iterator
+from typing import Any, Callable, Optional
+
 from ts_flow.core import Handler, T, U
-from typing import Callable, Optional, Any, Iterable, Iterator
+
 
 class MappingHandler(Handler[T, U]):
-    def __init__(
-        self,
-        map_func: Callable[[T], U],
-        source: Optional[Handler[Any, T]] = None
-    ):
+    def __init__(self, map_func: Callable[[T], U], source: Optional[Handler[Any, T]] = None):
         super().__init__(source)
         self.map_func = map_func
 
     def __iter__(self) -> Iterator[U]:
         if self.source is None:
             raise ValueError("Source is not set")
-        
+
         for segment in self.source:
             yield self.map_func(segment)
