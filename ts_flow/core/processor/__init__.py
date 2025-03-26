6b24f5efc021b176745437a0012402cd4b9eca0f
Rustam Shangareev <rustam.letitbe@gmail.com>
2025-03-26 20:23:12 +0300

    feat: add new components to processor init file

---
+++ ts_flow/core/processor/__init__.py
@@ -0,0 +1,11 @@
+from .filter_handler import OfflineFilterHandler, OnlineFilterHandler
+from .mapping_handler import MappingHandler
+from .sampling_handler import OfflineSamplingHandler, OnlineSamplingHandler
+
+__all__ = [
+    "MappingHandler",
+    "OfflineFilterHandler",
+    "OfflineSamplingHandler",
+    "OnlineFilterHandler",
+    "OnlineSamplingHandler",
+]
