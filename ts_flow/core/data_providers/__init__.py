178acc63d9eb6590fcf89797827b484ab4f01fb5
Rustam Shangareev <rustam.letitbe@gmail.com>
2025-03-29 20:40:05 +0300

    refactor: add pre-commit changes

--- ts_flow/core/data_providers/__init__.py
+++ ts_flow/core/data_providers/__init__.py
@@ -1,5 +1,5 @@
 from .abstract import DataProvider, T
-from .simple_data_provider import SimpleDataProvider
 from .file_data_provider import FileDataProvider
+from .simple_data_provider import SimpleDataProvider
 
-__all__ = ["DataProvider", "SimpleDataProvider", "FileDataProvider", "T"]
+__all__ = ["DataProvider", "FileDataProvider", "SimpleDataProvider", "T"]
